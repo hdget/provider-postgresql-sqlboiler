9047b11fe40ecbbf89bf9bd4d31be7e838a055ca
Ryan Fan <reg_info@qq.com>
2025-11-04 09:35:52 +0800

    add NewClient (#8)

--- provider.go
+++ provider.go
@@ -3,6 +3,7 @@ package postgresql_sqlboiler
 import (
 	"github.com/aarondl/sqlboiler/v4/boil"
 	"github.com/hdget/common/types"
+	"github.com/pkg/errors"
 )
 
 type psqlProvider struct {
@@ -26,41 +27,66 @@ func New(configProvider types.ConfigProvider, logger types.LoggerProvider) (type
 	if config.Default != nil {
 		p.defaultDb, err = newClient(config.Default)
 		if err != nil {
-			logger.Fatal("init postgresql default connection", "err", err)
+			logger.Fatal("init postgresql default db connection", "err", err)
 		}
 
 		// 设置boil的缺省db
 		boil.SetDB(p.defaultDb)
-		logger.Debug("init postgresql default", "host", config.Default.Host)
+		logger.Debug("init postgresql default db connection", "host", config.Default.Host)
 	}
 
 	if config.Master != nil {
 		p.masterDb, err = newClient(config.Master)
 		if err != nil {
-			logger.Fatal("init postgresql master connection", "err", err)
+			logger.Fatal("init postgresql master db connection", "err", err)
 		}
-		logger.Debug("init postgresql master", "host", config.Master.Host)
+
+		logger.Debug("init postgresql master db connection", "host", config.Master.Host)
 	}
 
 	for i, slaveConf := range config.Slaves {
 		p.slaveDbs[i], err = newClient(slaveConf)
 		if err != nil {
-			logger.Fatal("init postgresql slave connection", "slave", i, "err", err)
+			logger.Fatal("init postgresql slave db connection", "slave", i, "err", err)
 		}
 
-		logger.Debug("init postgresql slave", "index", i, "host", slaveConf.Host)
+		logger.Debug("init postgresql slave db connection", "index", i, "host", slaveConf.Host)
 	}
 
 	for _, extraConf := range config.Items {
 		p.extraDbs[extraConf.Name], err = newClient(extraConf)
 		if err != nil {
-			logger.Fatal("new postgresql extra connection", "name", extraConf.Name, "err", err)
+			logger.Fatal("new postgresql extra db connection", "name", extraConf.Name, "err", err)
 		}
 
-		logger.Debug("init postgresql extra", "name", extraConf.Name, "host", extraConf.Host)
+		logger.Debug("init postgresql extra db connection", "name", extraConf.Name, "host", extraConf.Host)
 	}
 
 	return p, nil
+}
+
+func NewClient(configProvider types.ConfigProvider) (types.DbClient, error) {
+	config, err := newConfig(configProvider)
+	if err != nil {
+		return nil, errors.Wrap(err, "new postgresql config")
+	}
+
+	var c *psqlConfig
+	if config.Default != nil {
+		c = config.Default
+	} else if config.Master != nil {
+		c = config.Master
+	} else {
+		return nil, errors.New("postgresql config not found")
+	}
+
+	// 强制性使用系统默认数据库
+	c.Database = "postgres"
+	client, err := newClient(c)
+	if err != nil {
+		return nil, errors.Wrap(err, "init postgresql sys db connection")
+	}
+	return client, nil
 }
 
 func (p *psqlProvider) GetCapability() types.Capability {
